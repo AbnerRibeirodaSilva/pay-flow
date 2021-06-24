fee54052494a4a6934c8d1d84a9244808bb9f5f9
Abner Ribeiro <sabner838@gmail.com>
2021-06-24 01:41:47 -0300

    Sistema de autenticação finalizado

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/app_widget.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(AppFirebase());
+}
+
+class AppFirebase extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _AppFirebaseState createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Material(
+            child: Center(
+              child: Text(
+                'Não foi possivel inicializar o Firebase',
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        }
+        return Material(
+          child: Center(
+            child: CircularProgressIndicator(
+              valueColor: new AlwaysStoppedAnimation<Color>(AppColors.grey),
+              color: AppColors.primary,
+              backgroundColor: AppColors.background,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
